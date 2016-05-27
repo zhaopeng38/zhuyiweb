b1672c0dae1a83288b4f9f2767b1b970eccce3c4
peng zhao <peng.zhao@condenast.com.cn>
2016-05-27 17:45:15 +0800

    注释视频

--- controllers/VideocolController.php
+++ controllers/VideocolController.php
@@ -1,7 +1,7 @@
 <?php
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
+ * 视频合集类
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
